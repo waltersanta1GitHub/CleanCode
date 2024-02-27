00292e6ad40ef5f0bb3d30cf97ab8791e24e5dcd
Jose Walter Santamaria <waltersanta1@hotmail.com>
2024-02-27 13:56:50 +0100

    Add Primitives

---
+++ CleanPOS/Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,15 @@
+namespace Domain.Primitives;
+
+public abstract class AggregateRoot
+{
+
+    private readonly List<DomainEvent> _domainEvents = new();
+
+    public ICollection<DomainEvent> GetDomainEvents() => _domainEvents;
+
+    protected void Raise(DomainEvent domainEvent){
+
+        _domainEvents.Add(domainEvent);
+    }
+
+}
